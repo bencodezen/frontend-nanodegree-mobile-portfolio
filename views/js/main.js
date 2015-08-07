ef7684c2632744ef887da7f0677f129e69c419ab
Ben Hong <bencodezen@gmail.com>
2015-08-07 13:56:23 -0400

    Provide fallback for will-change: transform

--- views/js/main.js
+++ views/js/main.js
@@ -500,7 +500,6 @@ function updatePositions() {
   for (var i = 0; i < items.length; i++) {
     var phase = Math.sin(scrollAmount + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
-    // items[i].style.transform = 'translateX(' + items[i].basicLeft + 100 * phase + 'px)';
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
